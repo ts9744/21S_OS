b3fa3ddf34339cd4ab440c65e7385b0e6d05214b
TaeSu Kim <166081401+ts9744@users.noreply.github.com>
2024-06-06 20:46:27 +0900

    Update crawlcode.py

--- crawlcode.py
+++ crawlcode.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ing: utf-8 -*-
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,7 +8,7 @@ def get_links_with_keyword(url, keyword):
         # HTTP 요청
         response = requests.get(url)
         response.raise_for_status()  # HTTP 오류가 발생하면 예외를 발생시킵니다.
-        print("HTTP 요청 성공, 상태 코드:", response.status_code)
+        print(f"HTTP 요청 성공, 상태 코드: {response.status_code}")
         
         # HTML 파싱
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,7 +22,7 @@ def get_links_with_keyword(url, keyword):
         links = []
         for post in posts:
             if keyword in post.text:
-                print(f"키워드 포함된 게시물: {post.text}")
+                print(f"키워드 포함된 게시물: {post.text.strip()}")
                 links.append(post['href'])
 
         if not links:
@@ -40,10 +40,12 @@ def get_links_with_keyword(url, keyword):
 url = 'https://edu.dcinside.com/board/lists/?id=extra_new1'
 keyword = '루틴'
 
+print("스크랩 시작")
 # 링크 스크랩
 links = get_links_with_keyword(url, keyword)
 
-# 결과 출력
 print(f"스크랩된 링크 수: {len(links)}")
+# 결과 출력
 for link in links:
     print(link)
+print("스크랩 종료")
